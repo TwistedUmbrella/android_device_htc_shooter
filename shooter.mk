9bf928757debc8168b08de1b19334b9cdeb7b553
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-22 21:57:32 -0400

    Remove shared KSM configuration to ensure it is picked up

--- shooter.mk
+++ shooter.mk
@@ -23,12 +23,6 @@ $(call inherit-product-if-exists, vendor/htc/shooter/shooter-vendor.mk)
 # Inherit Language Files
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := htc_shooter
-PRODUCT_DEVICE := shooter
-PRODUCT_MODEL := HTC Evo 3D
-PRODUCT_MANUFACTURER := HTC
-
 # Set up the product codename, build version & MOTD.
 #PRODUCT_CODENAME := e3d
 #PRODUCT_VERSION_DEVICE_SPECIFIC := alpha10
@@ -156,6 +150,9 @@ $(call inherit-product, device/htc/common/common.mk)
 
 $(call inherit-product, build/target/product/full_base.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_BRAND := HTC
 PRODUCT_NAME := htc_shooter
 PRODUCT_DEVICE := shooter
-PRODUCT_MODEL := Full Android on Shooter
+PRODUCT_MODEL := HTC Evo 3D
+PRODUCT_MANUFACTURER := HTC
