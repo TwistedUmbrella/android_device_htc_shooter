384a7ac20bfd4b81961b145aa4661f94e5c3771b
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-02 11:41:51 -0400

    Realign build dates and device profile information

--- shooter.mk
+++ shooter.mk
@@ -144,9 +144,8 @@ $(call inherit-product, device/htc/common/common.mk)
 
 $(call inherit-product, build/target/product/full_base.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_BRAND := HTC
 PRODUCT_NAME := htc_shooter
 PRODUCT_DEVICE := shooter
-PRODUCT_MODEL := HTC Evo 3D
+PRODUCT_MODEL := PG86100
 PRODUCT_MANUFACTURER := HTC
