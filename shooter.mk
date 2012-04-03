69afec8359589cd5560299feeb5caab5a75d21cf
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-03 19:11:48 -0400

    Remove separated proprietary camera configuration layout

--- shooter.mk
+++ shooter.mk
@@ -19,7 +19,6 @@
 
 ## (2) Also get non-open-source GSM-specific aspects if available
 $(call inherit-product-if-exists, vendor/htc/shooter/shooter-vendor.mk)
-$(call inherit-product-if-exists, vendor/htc/shooter/shooter-camera.mk)
 
 # Inherit Language Files
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
